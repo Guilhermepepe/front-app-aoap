f7fd0fae1746d80f7a987e89b5e888552c7ae6c6
Guilhermepepe <71614960+Guilhermepepe@users.noreply.github.com>
2021-06-25 19:05:47 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'http://192.168.1.41:3000'
+    baseURL: 'http://ec2-3-142-238-1.us-east-2.compute.amazonaws.com/'
 })
 
 export default instance
